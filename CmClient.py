559032ce6db766adb20b992605c78d620afd6688
AnyFlyGuy <stephan.laschet@gmail.com>
2020-06-11 11:22:46 +0200

    refactor OAuth session to CmSession

--- CmClient.py
+++ CmClient.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python38
+#!/usr/bin/env python3
 
 
 import json
 import logging
 
-import requests
-from requests_oauthlib import OAuth1Session, OAuth1
+from CmSession import CmSession
 
 
 class CmClient:
@@ -19,7 +18,6 @@ class CmClient:
             self.logger = logging.getLogger('local_client_logger')
         else:
             self.logger = logger
-
         if config_data is not None:
             self.config = config_data
         else:
@@ -27,62 +25,35 @@ class CmClient:
                 self.config = json.load(json_config)
         with open("./data/confidential_config.json", "r") as json_confidential_config:
             self.confidential_config = json.load(json_confidential_config)
-
-    def generate_full_url(self, url, url_ext):
-        if url is None:
-            if url_ext is None:
-                raise ValueError("No URL and no URL Extension given, can't request anything.")
-            else:
-                url = "{}{}".format(self.config["urls"]["base_url"], url_ext)
-        return url
-
-    def update_client_session_url(self, url=None, url_ext=None, ):
-        url = self.generate_full_url(url, url_ext)
-        self.api_client = OAuth1Session(self.confidential_config["cm_access"]["app_token"],
-                                        client_secret=self.confidential_config["cm_access"]["app_secret"],
-                                        resource_owner_key=self.confidential_config["cm_access"]["access_token"],
-                                        resource_owner_secret=self.confidential_config["cm_access"]["access_secret"],
-                                        realm=url
-                                        )
-
-    def get_data(self, url=None, url_ext=None, params=None):
-        url = self.generate_full_url(url, url_ext)
-        return self.api_client.get(url, params=params)
+        self.cm_session = CmSession(self.config["urls"]["base_url"], self.confidential_config)
 
     def get_account_articles(self):
         url_ext = "/users/{}/articles".format(self.confidential_config["account"]["user_name"])
-        self.update_client_session_url(url_ext=url_ext)
-        return self.get_data(url_ext=url_ext)
+        self.cm_session.update_client_session_url(url_ext=url_ext)
+        return self.cm_session.get_data(url_ext=url_ext)
 
     def get_account_stock(self):
         url_ext = "/stock"
-        self.update_client_session_url(url_ext=url_ext)
-        return self.get_data(url_ext=url_ext)
+        self.cm_session.update_client_session_url(url_ext=url_ext)
+        return self.cm_session.get_data(url_ext=url_ext)
 
     def get_account_data(self):
         url_ext = "/account"
-        self.update_client_session_url(url_ext=url_ext)
-        return self.get_data(url_ext=url_ext)
+        self.cm_session.update_client_session_url(url_ext=url_ext)
+        return self.cm_session.get_data(url_ext=url_ext)
 
     def get_card_info(self, product_id):
         url_ext = "/products/{}".format(product_id)
-        self.update_client_session_url(url_ext=url_ext)
-        return self.get_data(url_ext=url_ext)
-
-    def debug_pretty_print_json(self, response):
-        json_object = json.loads(response.text)
-        json_formatted_str = json.dumps(json_object, indent=3)
-        self.logger.debug(json_formatted_str)
-
-    def _prettify_response(self, response):
-        request_ok = response.status_code == 200
-        if not request_ok:
-            self.logger.error(response.reason)
-            return request_ok, {}
-        response_data = json.loads(response.text)
-        return request_ok, response_data
+        self.cm_session.update_client_session_url(url_ext=url_ext)
+        return self.cm_session.get_data(url_ext=url_ext)
 
 
 if __name__ == "__main__":
     client = CmClient(None)
-    client.debug_pretty_print_json(client.get_account_stock())
+    response = client.get_account_stock()
+    if response.status_code == 200 or response.status_code == 206:
+        json_object = json.loads(response.text)
+        json_formatted_str = json.dumps(json_object, indent=3)
+        client.logger.debug(json_formatted_str)
+    else:
+        client.logger.error("Faulty response {}".format(response.status_code))
---
+++ CmSession.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from requests_oauthlib import OAuth1Session, OAuth1
+
+
+class CmSession:
+
+    def __init__(self, base_url, confidential_config):
+        self.api_client = None
+        self.base_url = base_url
+        self.confidential_config = confidential_config
+
+    def generate_full_url(self, url, url_ext):
+        if url is None:
+            if url_ext is None:
+                raise ValueError("No URL and no URL Extension given, can't request anything.")
+            else:
+                url = "{}{}".format(self.base_url, url_ext)
+        return url
+
+    def update_client_session_url(self, url=None, url_ext=None, ):
+        url = self.generate_full_url(url, url_ext)
+        self.api_client = OAuth1Session(self.confidential_config["cm_access"]["app_token"],
+                                        client_secret=self.confidential_config["cm_access"]["app_secret"],
+                                        resource_owner_key=self.confidential_config["cm_access"]["access_token"],
+                                        resource_owner_secret=self.confidential_config["cm_access"]["access_secret"],
+                                        realm=url
+                                        )
+
+    def get_data(self, url=None, url_ext=None, params=None):
+        url = self.generate_full_url(url, url_ext)
+        return self.api_client.get(url, params=params)
+
+
+if __name__ == "__main__":
+    pass
