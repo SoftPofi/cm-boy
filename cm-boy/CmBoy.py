e108c263f316732428b02a8fe8de02a7f1ce7634
Eszter Iklodi <eszter.iklodi@mercateo.com>
2020-06-19 09:04:54 +0200

    Test CmClient

--- cm-boy/CmBoy.py
+++ cm-boy/CmBoy.py
@@ -11,8 +11,3 @@ class CmBoy:
         with open(".data/config.json", "r") as json_config:
             self.config = json.load(json_config)
         self.cm_interface = CmClient(self.config)
-
-
-# Todo: delete
-if __name__ == "__main__":
-    pass
--- tests/test_CmClient.py
+++ tests/test_CmClient.py
@@ -1,28 +1,45 @@
 from unittest import TestCase
 
+from CmClient import CmClient
+
 
 class TestCmClient(TestCase):
 
+    def setUp(self):
+        self.uut = CmClient(None)
+        self.product_id = 27
+
     def test_get_account_articles(self):
-        # Todo
-        pass
+        resp = self.uut.get_account_articles()
+        self.assertResponseOK(resp)
 
     def test_get_account_stock(self):
-        # Todo
-        pass
+        resp = self.uut.get_account_stock()
+        self.assertResponseOK(resp)
 
     def test_get_account_data(self):
-        # Todo
-        pass
+        resp = self.uut.get_account_data()
+        self.assertResponseOK(resp)
 
     def test_get_card_info(self):
-        # Todo
-        pass
+        resp = self.uut.get_card_info(self.product_id)
+        self.assertResponseOK(resp)
 
     def test_get_card_listing(self):
-        # Todo
-        pass
+        resp = self.uut.get_card_listing(self.product_id)
+        self.assertResponseOK(resp)
 
     def test_put_card_price(self):
-        # Todo
-        pass
+        example_card = {
+            "article":
+                {
+                    "idArticle": "593000831",
+                    "count": 1,
+                    "price": 0.44,
+                }
+        }
+        resp = self.uut.put_card_price(example_card)
+        self.assertResponseOK(resp)
+
+    def assertResponseOK(self, resp):
+        self.assertIn(resp.status_code, [200, 206])
