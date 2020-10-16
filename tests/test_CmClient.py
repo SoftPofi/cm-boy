248884d1ddf8cae03593e9368b95468fcec82197
AnyFlyGuy <stephan.laschet@gmail.com>
2020-10-16 14:33:24 +0200

    remove Upload Test, since system is live and this uploads cards with wrong prices to account

--- tests/test_CmClient.py
+++ tests/test_CmClient.py
@@ -31,10 +31,3 @@ class TestCmClient(TestCase):
         resp, _, _ = self.uut.get_card_listing(self.product_id)
         self.assertTrue(resp)
 
-    def test_put_card_price(self):
-        example_card = {
-            "article":
-                {'idArticle': 593000831, 'idProduct': 27, 'language': {'idLanguage': 1, 'languageName': 'English'}, 'comments': '', 'price': 0.40, 'idCurrency': 1, 'currencyCode': 'EUR', 'count': 1, 'inShoppingCart': False, 'lastEdited': '2020-06-11T13:51:42+0200', 'product': {'idGame': 1, 'image': '//static.cardmarket.com/img/6058ee20479ba5c80d41447a9b1625fc/items/1/MRD/27.jpg', 'enName': 'Taj-Nar Swordsmith', 'locName': 'Taj-Nar Swordsmith', 'expansion': 'Mirrodin', 'nr': '27', 'expIcon': 43, 'rarity': 'Uncommon'}, 'condition': 'EX', 'isFoil': False, 'isSigned': False, 'isPlayset': False, 'isAltered': False}
-        }
-        resp, _, _ = self.uut.put_card_price(example_card)
-        self.assertTrue(resp)
