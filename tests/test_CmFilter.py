8177147552c0ccdeb98d3f5712fd17429a29cc4e
AnyFlyGuy <stephan.laschet@gmail.com>
2021-02-10 21:49:30 +0100

    use testdataprovider instead of file paths in CmFilter

--- tests/test_CmFilter.py
+++ tests/test_CmFilter.py
@@ -11,12 +11,9 @@ class test_CmFilter(TestCase):
     def setUp(self):
         test_data = TestDataProvider()
         self.uut = CmFilter(config=test_data.provide_config())
-        with open("./test_data/example_card_product_id_27.json", "r") as json_test_card:
-            self.test_card = json.load(json_test_card)
-        with open("./test_data/listing_product_id_27_entries_20.json", "r") as json_test_listing:
-            self.listing = json.load(json_test_listing)
-        with open("./test_data/listing_product_id_27_filtered_entries_20.json", "r") as json_expected_result:
-            self.expected_result = json.load(json_expected_result)
+        self.test_card = test_data.provide_example_card_id_27()
+        self.listing = test_data.provide_listing_id_27()
+        self.expected_result = test_data.provide_filtered_listing_id_27()
 
     def test_prefilter(self):
         self.uut.prefilter(self.listing, self.test_card)
