035188ba8c6c2e2cc1e18bd98cf8d67a578e7d92
AnyFlyGuy <stephan.laschet@gmail.com>
2021-03-10 13:53:21 +0100

    add cd before calling CmLeash.py

--- cron_job.sh
+++ cron_job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Start cron job"
 screen -ls | grep '(Detached)' | awk '{print $1}' | xargs -I % -t screen -X -S % quit
-screen -dm bash -c 'python3 CmLeash.py -f -o'
+screen -dm bash -c 'cd $cm_base_dir; python3 CmLeash.py -f -o'
