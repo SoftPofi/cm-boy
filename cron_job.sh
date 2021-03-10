1f4b441a28dd9eae00b9f65c26fbc03db3798cea
AnyFlyGuy <stephan.laschet@gmail.com>
2021-03-10 21:24:10 +0100

    add echos to reflect execution

--- cron_job.sh
+++ cron_job.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "Start cron job"
+echo "Remove running screen sessions"
 screen -ls | grep '(Detached)' | awk '{print $1}' | xargs -I % -t screen -X -S % quit
+echo "Run CmLeash in screen"
 screen -L -Logfile ./log/screen.log -dm bash -c 'cd $cm_base_dir; git checkout main; git pull; python3 CmLeash.py -f -o'
+echo "Done"
